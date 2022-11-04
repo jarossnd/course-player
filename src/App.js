8656c472040496beacc3962fc2ed70f4cd32f5d9
Jason Ross <jason@jasonross.xyz>
2022-11-04 07:12:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player/vimeo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Course Title</h1>
+      <h2>Course Player</h2>
+      <ReactPlayer url='https://vimeo.com/766260846' controls='true'/>
+      <h2>Play List</h2>
     </div>
   );
 }
---
+++ src/components/Player.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactPlayer from 'react-player/vimeo'
+
+function Player() {
+    // Render a YouTube video player
+    <ReactPlayer url='https://vimeo.com/766260846' />
+}
+
+export default Player;
